39d03791972a59f097c217d2d448075d5eb36694
rohullahkarimi <rohullahahmad99@gmail.com>
2019-01-22 10:38:04 +0200

    Add files via upload

---
+++ pdo.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=vvSovellus','root',"", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    
+catch(Exception $e) {
+    echo $e->getMessage();
+    }
+?>
